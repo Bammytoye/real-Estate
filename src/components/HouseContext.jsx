c5a3999877b89b900a385a2bad4f6b26eeace3b3
bammytoye <bamigbalatoyese@gmail.com>
2025-02-14 20:54:52 +0100

    loading icons

--- src/components/HouseContext.jsx
+++ src/components/HouseContext.jsx
@@ -27,7 +27,79 @@ const HouseContextProvider = ({ children }) => {
     }, [houses]); 
     
     const handleClick = () => {
-        console.log('handleClick')
+        //set loading
+        setLoading(true);
+
+        //create a function that check if the string include any
+        const isDefault = (str) => {
+            return str.split(' ').includes('(any)');
+        };
+
+        //get first value of the price and parse it to number
+        const minPrice = parseInt(price.split(' ')[0]);
+
+        //get the second value of the price 
+        const maxPrice = parseInt(price.split(' ')[2]);
+
+        const newHouses = housesData.filter((house) => {
+            const housePrice = parseInt(house.price);
+
+            //if all values are selected
+            if (
+                house.country === country &&
+                house.type === property && 
+                housePrice >= minPrice &&
+                housePrice <= maxPrice
+            ) {
+                return house;
+            }
+
+            //if all values are default
+            if (isDefault(country) && isDefault(property) && isDefault(price)) {
+                return house;
+            }
+
+            //if all country are default
+            if (isDefault(country) && isDefault(property) && isDefault(price)) {
+                return house.country === country;
+            }
+
+            //if all property are default
+            if (isDefault(property) && isDefault(country) && isDefault(price)) {
+                return house.type === property;
+            }
+
+            //if all price are default
+            if (isDefault(price) && isDefault(country) && isDefault(property)) {
+                if (housePrice >= minPrice && housePrice <= maxPrice) {
+                    return house
+                };
+            }
+
+            //if country and property is not default
+            if (isDefault(country) && isDefault(property) && isDefault(price)) {
+                return house.country === country && house.type === property; 
+            }
+
+            //if country and price is not default
+            if (isDefault(country) && isDefault(property) && isDefault(price)) {
+                if (housePrice >= minPrice && housePrice <= maxPrice) {
+                    return house.country === country
+                }
+            }
+
+            //if property and price is not default
+            if (isDefault(country) && isDefault(property) && isDefault(price)) {
+                if (housePrice >= minPrice && housePrice <= maxPrice) {
+                    return house.type === property;
+                }
+            }
+        });
+
+        setTimeout(() => {
+            return newHouses.length < 1 ? setHouses([]) : setHouses(newHouses);
+            setLoading(false); 
+        }, 1000)
     }
 
     return (
@@ -45,6 +117,7 @@ const HouseContextProvider = ({ children }) => {
             loading,
             setLoading, 
             handleClick,
+            loading,
         }}>
             {children}
         </HouseContext.Provider>
--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -2,11 +2,19 @@ import React, { useContext } from 'react'
 import { HouseContext } from './HouseContext'
 import House from './House'
 import { Link } from 'react-router' 
-import { ImSpinner2 } from 'react-icons'
+import { ImSpinner2 } from 'react-icons/im'
 
 function HouseList() {
     const {houses, loading} = useContext(HouseContext);
-    console.log(houses)
+
+    //if the loading is true
+    if (loading) {
+        return (
+            <ImSpinner2 
+                className='mx-auto animate-spin text-violet-700 text-4xl mt-[200px]'
+            />
+        )
+    }
 
     return (
         <section className='mb-20'>
